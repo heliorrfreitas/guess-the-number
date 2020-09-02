9fa6b1b02196016cc81ce6c6e4f3fc82e9c4ebf7
heliorrfreitas <heliorui.ramosfreitas@gus.pittstate.edu>
2020-09-01 22:36:05 -0500

    Add tips functions to game. Add README.md file

--- guess-the-number.lisp
+++ guess-the-number.lisp
@@ -1,10 +1,26 @@
+(defparameter *tips* nil)
 (defun play-guess-number-game () 
-	(print "It is your turn to guess the number. The computer will generate a random  number between 0 and 100. Use the best method for your guesses. to guess use the command (my-guess 10) the number 10 is a example of value you can enter.")
+	(print "It is your turn to guess the number. The computer will generate a random  number between 0 and 100. Use the best method for your guesses. to guess use the command (my-guess 10) the number 10 is a example of value you can enter. The game starts with tips off by default. If you want to turn on the tips in the game use (tips-on) .")
+	
 	(defparameter *number* (random 100) ))
 
 (defun my-guess (user-guess)
 	(if (eq *number* user-guess)
 		'victory
-		'try-again  ))
+		(if *tips* 
+			(if (> user-guess *number*)
+				'smaller
+				'bigger )
+			'try-again )))
+
+(defun tips-on ()
+	(print "tips turned on")
+	(defparameter *tips* 1))
+
+(defun tips-off ()
+	(print "tips turned off")
+	(defparameter *tips* nil))
+	
+
 
 
